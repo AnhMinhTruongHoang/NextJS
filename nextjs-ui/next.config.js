8a8b39f081d7f0adcff1d239b4a917009834a202
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-07-07 02:01:34 +0700

    fix bug

--- nextjs-ui/next.config.js
+++ nextjs-ui/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   swcMinify: true,
   modularizeImports: {
     "@mui/icons-material": {
@@ -9,12 +10,12 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "",
-        pathname: "/random",
-      },
+      // {
+      //   protocol: "http",
+      //   hostname: "host.docker.internal",
+      //   port: "8001",
+      //   pathname: "/images/**",
+      // },
       {
         protocol: "http", // s vì bạn dùng http ở localhost
         hostname: "localhost",
